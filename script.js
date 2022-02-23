8664766048d63ffb4cf82c38b0c5146b91ca6876
MattFofo <matteo.segattini2@gmail.com>
2022-02-23 15:19:28 +0100

    scritta funzione per selezionare i multipli di 3, 5 e 15

--- script.js
+++ script.js
@@ -9,10 +9,32 @@ for (let i = 1; i < 101; i++) {
     let elementBox = document.createElement('div');
     elementBox.classList.add('box');
     elementBox.innerHTML = i;
+
+
+    // funzione per selezionare i multipli di 3, 5 , 15
+
+    if (i % 3 == 0 && i % 5 != 0) {
+        elementBox.classList.add('green');
+        elementBox.innerHTML = 'Fizz'
+
+    }else if (i % 5 == 0 && i % 3 != 0 ) {
+        elementBox.classList.add('yellow');
+        elementBox.innerHTML = 'Buzz'
+            
+    }else if (i % 15 == 0 ) {
+        elementBox.classList.add('blue');
+        elementBox.innerHTML = 'FizzBuzz'
+
+    }
+
     
+
+    
+    
+    console.log(elementBox);
 
     mainContent.append(elementBox);
-    console.log(i);    
     
-
+    
+        
 }
